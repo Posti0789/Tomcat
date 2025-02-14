f73be101d35ef152f25d445692962b12c129e094
Posti0789 <tonipostigos@gmail.com>
2025-02-14 16:34:34 +0100

    Comentarios

--- js/armas.js
+++ js/armas.js
@@ -41,7 +41,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
         armasContainer.appendChild(armaElemento);
 
-        // Easter Egg Mejorado 
+         
         if (arma.nombre === "Rueda de Logarius") {
             armaElemento.addEventListener("click", () => activarEasterEgg());
         }
